81d2a57dacaad08489bbb6a9b442be5645e66937
romel <romellocaccamisi@hotmail.com>
2019-11-07 15:57:17 +0100

    Adding co-prime verification related functions

--- js/script.js
+++ js/script.js
@@ -40,3 +40,72 @@ $(function () {
     });
 
 })
+
+/* Détermine si deux entiers <integer1> et <integer2> sont premiers */
+function areCoPrime(integer1, integer2)
+{
+    let integer1PrimeFactors = primeFactorsDecomposition(integer1);
+    let integer2PrimeFactors = primeFactorsDecomposition(integer2);
+    let commonPrimeFactors = commonSubArray(integer1PrimeFactors, integer2PrimeFactors);
+    return commonPrimeFactors.length == 0;
+}
+
+/* Retourne un tableau contenant les facteurs premiers de <integer> */
+function primeFactorsDecomposition(integer)
+{
+    let primeFactors = [];
+
+    for(let factor = 2; factor <= integer; factor++)
+    {
+        if (isDivisibleBy(integer, factor)) 
+        {
+            let isPrimeFactor = true;
+            for(let divisor = 2; isPrimeFactor && divisor <= factor / 2; divisor++) 
+                isPrimeFactor = !isDivisibleBy(factor, divisor);
+            
+            if(isPrimeFactor) 
+                while (isDivisibleBy(integer, factor)) 
+                {
+                    integer /= factor;
+                    primeFactors.push(factor);
+                }
+        }   
+    }
+
+    return primeFactors;
+}
+
+/* Retourne le sous-tableau commun le plus long de <array1> et <array2> */
+function commonSubArray(array1, array2)
+{
+    let lowIndexWithMaxSize;
+    let highIndexWithMaxSize;
+    let maxSize = 0;
+
+    for(let i = 0; i < array1.length; i++)
+    {
+        let size;
+        let savedICellArray1 = i;
+        let lowIndex = i;
+
+        for(let j = 0; j < array2.length && array1[i] == array2[j]; j++)
+            i++;
+
+        size = i - lowIndex;
+        if(size > maxSize) 
+        {
+            maxSize = size;
+            highIndexWithMaxSize = i;
+            lowIndexWithMaxSize = lowIndex;
+        }
+        i = savedICellArray1;
+    }
+
+    return array1.slice(lowIndexWithMaxSize, highIndexWithMaxSize);
+}
+
+/* Détermine si <integer> est divisible par <divisor> */
+function isDivisibleBy(integer, divisor)
+{
+    return integer % divisor == 0;
+}
